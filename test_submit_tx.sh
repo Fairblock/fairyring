47f641faa80baccf34241a53dfd85bf03c4f7b4f
p0p3yee <devp0p3yee@gmail.com>
2023-02-06 14:54:28 -0500

    Update submit encrypted tx script

--- test_submit_tx.sh
+++ test_submit_tx.sh
@@ -5,16 +5,41 @@ die () {
     exit 1
 }
 
-[ "$#" -eq 3 ] || die "3 argument required, $# provided, Usage: ./test_tx_execution {data_tx_target_height} {current sequence + 1} {tx_target_height}"
+[ "$#" -eq 2 ] || die "2 argument required, $# provided, Usage: ./test_tx_execution {data_tx_target_height} {tx_target_height}"
 
 echo $1 | grep -E -q '^[0-9]+$' || die "Numeric argument required, $1 provided"
 echo $2 | grep -E -q '^[0-9]+$' || die "Numeric argument required, $2 provided"
-echo $3 | grep -E -q '^[0-9]+$' || die "Numeric argument required, $3 provided"
 
-fairyringd tx fairblock submit-encrypted-tx "test-encrypted-tx-data" $1 --from alice --generate-only --yes > unsigned.json
-SIGNED_DATA=`fairyringd tx sign unsigned.json --from alice --offline --account-number 0 --sequence $2 --yes`
+ACCOUNT_NAME="alice"
+ACCOUNT_NUMBER=0
+CHAIN_ID="fairyring"
+UNSIGNED_TX_FILE_NAME="script_unsigned.json"
 
-fairyringd tx fairblock submit-encrypted-tx "$SIGNED_DATA" $3 --from alice --yes
+# Get the address of target account name
+ADDRESS=`fairyringd keys show $ACCOUNT_NAME | grep "address:" | sed 's/^.*: //'`
 
-echo "\n\nList encrypted Txs:"
+# Get the fairblock nonce of target account address
+FairblockNonce=`fairyringd query fairblock show-fairblock-nonce $ADDRESS | grep "nonce:" | sed 's/^.*: //'`
+
+# Check if get nonce is success, if not assign 1 to the nonce
+if [ -z "${FairblockNonce}" ]; then
+  echo "$ACCOUNT_NAME nonce not found, init nonce as 1"
+  FairblockNonce=1
+else # else, remove the string quote from the result
+  FairblockNonce=`sed -e 's/^"//' -e 's/"$//' <<< "$FairblockNonce"`
+fi
+
+printf "Got $ACCOUNT_NAME's FairblockNonce: $FairblockNonce \n\n"
+
+# Create the unsigned tx data
+fairyringd tx fairblock submit-encrypted-tx "test-encrypted-tx-data" $1 --from $ACCOUNT_NAME --generate-only --yes > $UNSIGNED_TX_FILE_NAME
+
+# Sign the unsigned tx that just created
+SIGNED_DATA=`fairyringd tx sign $UNSIGNED_TX_FILE_NAME --from $ACCOUNT_NAME --offline --account-number $ACCOUNT_NUMBER --sequence $FairblockNonce --chain-id $CHAIN_ID --yes`
+
+# Submit encrypted tx with the signed data
+fairyringd tx fairblock submit-encrypted-tx $SIGNED_DATA $2 --from $ACCOUNT_NAME --yes
+
+# List all the encrypted txs
+printf "\n\nList encrypted Txs:\n"
 fairyringd query fairblock list-encrypted-tx
