b10597fdbfcf114b02cba94a7324c25f892e2ef0
p0p3yee <devp0p3yee@gmail.com>
2023-03-09 14:35:33 -0500

    Remove error in tests file

--- x/fairblock/client/cli/tx_aggregated_key_share_test.go
+++ x/fairblock/client/cli/tx_aggregated_key_share_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
 
 	"fairyring/testutil/network"
@@ -52,128 +51,6 @@ func TestCreateAggregatedKeyShare(t *testing.T) {
 			args = append(args, fields...)
 			args = append(args, tc.args...)
 			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateAggregatedKeyShare(), args)
-			if tc.err != nil {
-				require.ErrorIs(t, err, tc.err)
-			} else {
-				require.NoError(t, err)
-				var resp sdk.TxResponse
-				require.NoError(t, ctx.Codec.UnmarshalJSON(out.Bytes(), &resp))
-				require.Equal(t, tc.code, resp.Code)
-			}
-		})
-	}
-}
-
-func TestUpdateAggregatedKeyShare(t *testing.T) {
-	net := network.New(t)
-	val := net.Validators[0]
-	ctx := val.ClientCtx
-
-	fields := []string{"xyz"}
-	common := []string{
-		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
-		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
-		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
-		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(net.Config.BondDenom, sdkmath.NewInt(10))).String()),
-	}
-	args := []string{
-		"0",
-	}
-	args = append(args, fields...)
-	args = append(args, common...)
-	_, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateAggregatedKeyShare(), args)
-	require.NoError(t, err)
-
-	for _, tc := range []struct {
-		desc     string
-		idHeight uint64
-
-		args []string
-		code uint32
-		err  error
-	}{
-		{
-			desc:     "valid",
-			idHeight: 0,
-
-			args: common,
-		},
-		{
-			desc:     "key not found",
-			idHeight: 100000,
-
-			args: common,
-			code: sdkerrors.ErrKeyNotFound.ABCICode(),
-		},
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			args := []string{
-				strconv.Itoa(int(tc.idHeight)),
-			}
-			args = append(args, fields...)
-			args = append(args, tc.args...)
-			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdUpdateAggregatedKeyShare(), args)
-			if tc.err != nil {
-				require.ErrorIs(t, err, tc.err)
-			} else {
-				require.NoError(t, err)
-				var resp sdk.TxResponse
-				require.NoError(t, ctx.Codec.UnmarshalJSON(out.Bytes(), &resp))
-				require.Equal(t, tc.code, resp.Code)
-			}
-		})
-	}
-}
-
-func TestDeleteAggregatedKeyShare(t *testing.T) {
-	net := network.New(t)
-
-	val := net.Validators[0]
-	ctx := val.ClientCtx
-
-	fields := []string{"xyz"}
-	common := []string{
-		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
-		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
-		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
-		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(net.Config.BondDenom, sdkmath.NewInt(10))).String()),
-	}
-	args := []string{
-		"0",
-	}
-	args = append(args, fields...)
-	args = append(args, common...)
-	_, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdCreateAggregatedKeyShare(), args)
-	require.NoError(t, err)
-
-	for _, tc := range []struct {
-		desc     string
-		idHeight uint64
-
-		args []string
-		code uint32
-		err  error
-	}{
-		{
-			desc:     "valid",
-			idHeight: 0,
-
-			args: common,
-		},
-		{
-			desc:     "key not found",
-			idHeight: 100000,
-
-			args: common,
-			code: sdkerrors.ErrKeyNotFound.ABCICode(),
-		},
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			args := []string{
-				strconv.Itoa(int(tc.idHeight)),
-			}
-			args = append(args, tc.args...)
-			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdDeleteAggregatedKeyShare(), args)
 			if tc.err != nil {
 				require.ErrorIs(t, err, tc.err)
 			} else {
