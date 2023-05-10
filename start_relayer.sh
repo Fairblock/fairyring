70cfca5602a7819e9ee27740ee187a16137ca5d0
p0p3yee <devp0p3yee@gmail.com>
2023-05-10 10:03:02 -0400

    Update fairyring config & relayer script

--- start_relayer.sh
+++ start_relayer.sh
@@ -5,14 +5,14 @@ ignite relayer configure -a \
   --source-faucet "http://0.0.0.0:4500" \
   --source-port "pep" \
   --source-version "pep-1" \
-  --source-gasprice "0.0000025stake" \
+  --source-gasprice "0.0000025frt" \
   --source-prefix "cosmos" \
   --source-gaslimit 300000 \
   --target-rpc "http://0.0.0.0:26659" \
   --target-faucet "http://0.0.0.0:4501" \
   --target-port "pep" \
   --target-version "pep-1" \
-  --target-gasprice "0.0000025stake" \
+  --target-gasprice "0.0000025token" \
   --target-prefix "auction" \
   --target-gaslimit 300000
 
