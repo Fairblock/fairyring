c69b6443115d686fa37127e85d6830d694198ef3
ap0calypse644 <nibirpal1994@gmail.com>
2023-10-26 14:56:19 +0530

    temporarily removing blockbuster for testing

--- app/app.go
+++ app/app.go
@@ -7,10 +7,6 @@ import (
 	"path/filepath"
 
 	"fairyring/blockbuster/abci"
-	"fairyring/blockbuster/lanes/base"
-
-	"fairyring/blockbuster"
-	"fairyring/blockbuster/lanes/keyshare"
 
 	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
 	reflectionv1 "cosmossdk.io/api/cosmos/reflection/v1"
@@ -34,7 +30,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/cosmos/cosmos-sdk/x/auth"
-	"github.com/cosmos/cosmos-sdk/x/auth/ante"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	authsims "github.com/cosmos/cosmos-sdk/x/auth/simulation"
 	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
@@ -587,97 +582,97 @@ func New(
 
 	keyshareIBCModule := keysharemodule.NewIBCModule(app.KeyshareKeeper)
 
-	// ---------------------------------------------------------------------------- //
-	// ------------------------- Begin Custom Code -------------------------------- //
-	// ---------------------------------------------------------------------------- //
+	// // ---------------------------------------------------------------------------- //
+	// // ------------------------- Begin Custom Code -------------------------------- //
+	// // ---------------------------------------------------------------------------- //
 
-	// Set fairyring's mempool into the app.
-	config := blockbuster.BaseLaneConfig{
-		Logger:        app.Logger(),
-		TxEncoder:     app.txConfig.TxEncoder(),
-		TxDecoder:     app.txConfig.TxDecoder(),
-		MaxBlockSpace: sdk.ZeroDec(),
-	}
+	// // Set fairyring's mempool into the app.
+	// config := blockbuster.BaseLaneConfig{
+	// 	Logger:        app.Logger(),
+	// 	TxEncoder:     app.txConfig.TxEncoder(),
+	// 	TxDecoder:     app.txConfig.TxDecoder(),
+	// 	MaxBlockSpace: sdk.ZeroDec(),
+	// }
 
-	// Create the lanes.
-	//
-	// NOTE: The lanes are ordered by priority. The first lane is the highest priority
-	// lane and the last lane is the lowest priority lane.
+	// // Create the lanes.
+	// //
+	// // NOTE: The lanes are ordered by priority. The first lane is the highest priority
+	// // lane and the last lane is the lowest priority lane.
 
-	// Keyshare lane allows for CreateAggrgatedKeyShare transactions to be processed before others.
-	keyshareLane := keyshare.NewKeyShareLane(
-		config,
-		0,
-		keyshare.NewDefaultKeyshareFactory(app.txConfig.TxDecoder()),
-	)
+	// // Keyshare lane allows for CreateAggrgatedKeyShare transactions to be processed before others.
+	// keyshareLane := keyshare.NewKeyShareLane(
+	// 	config,
+	// 	0,
+	// 	keyshare.NewDefaultKeyshareFactory(app.txConfig.TxDecoder()),
+	// )
 
-	// Default lane accepts all other transactions.
-	defaultConfig := blockbuster.BaseLaneConfig{
-		Logger:        app.Logger(),
-		TxEncoder:     app.txConfig.TxEncoder(),
-		TxDecoder:     app.txConfig.TxDecoder(),
-		MaxBlockSpace: sdk.ZeroDec(),
-		IgnoreList: []blockbuster.Lane{
-			keyshareLane,
-		},
-	}
-	defaultLane := base.NewDefaultLane(defaultConfig)
+	// // Default lane accepts all other transactions.
+	// defaultConfig := blockbuster.BaseLaneConfig{
+	// 	Logger:        app.Logger(),
+	// 	TxEncoder:     app.txConfig.TxEncoder(),
+	// 	TxDecoder:     app.txConfig.TxDecoder(),
+	// 	MaxBlockSpace: sdk.ZeroDec(),
+	// 	IgnoreList: []blockbuster.Lane{
+	// 		keyshareLane,
+	// 	},
+	// }
+	// defaultLane := base.NewDefaultLane(defaultConfig)
 
-	lanes := []blockbuster.Lane{
-		keyshareLane,
-		defaultLane,
-	}
+	// lanes := []blockbuster.Lane{
+	// 	keyshareLane,
+	// 	defaultLane,
+	// }
 
-	mempool := blockbuster.NewMempool(lanes...)
-	app.BaseApp.SetMempool(mempool)
+	// mempool := blockbuster.NewMempool(lanes...)
+	// app.BaseApp.SetMempool(mempool)
 
-	// Create a global ante handler that will be called on each transaction when
-	// proposals are being built and verified.
-	handlerOptions := ante.HandlerOptions{
-		AccountKeeper:   app.AccountKeeper,
-		BankKeeper:      app.BankKeeper,
-		FeegrantKeeper:  app.FeeGrantKeeper,
-		SigGasConsumer:  ante.DefaultSigVerificationGasConsumer,
-		SignModeHandler: app.txConfig.SignModeHandler(),
-	}
-	options := FairyringHandlerOptions{
-		BaseOptions:  handlerOptions,
-		PepKeeper:    app.PepKeeper,
-		TxDecoder:    app.txConfig.TxDecoder(),
-		TxEncoder:    app.txConfig.TxEncoder(),
-		KeyShareLane: keyshareLane,
-		Mempool:      mempool,
-	}
-	anteHandler := NewFairyringAnteHandler(options)
+	// // Create a global ante handler that will be called on each transaction when
+	// // proposals are being built and verified.
+	// handlerOptions := ante.HandlerOptions{
+	// 	AccountKeeper:   app.AccountKeeper,
+	// 	BankKeeper:      app.BankKeeper,
+	// 	FeegrantKeeper:  app.FeeGrantKeeper,
+	// 	SigGasConsumer:  ante.DefaultSigVerificationGasConsumer,
+	// 	SignModeHandler: app.txConfig.SignModeHandler(),
+	// }
+	// options := FairyringHandlerOptions{
+	// 	BaseOptions:  handlerOptions,
+	// 	PepKeeper:    app.PepKeeper,
+	// 	TxDecoder:    app.txConfig.TxDecoder(),
+	// 	TxEncoder:    app.txConfig.TxEncoder(),
+	// 	KeyShareLane: keyshareLane,
+	// 	Mempool:      mempool,
+	// }
+	// anteHandler := NewFairyringAnteHandler(options)
 
-	// Set the lane config on the lanes.
-	for _, lane := range lanes {
-		lane.SetAnteHandler(anteHandler)
-	}
+	// // Set the lane config on the lanes.
+	// for _, lane := range lanes {
+	// 	lane.SetAnteHandler(anteHandler)
+	// }
 
-	// Set the proposal handlers on the BaseApp along with the custom antehandler.
-	proposalHandlers := abci.NewProposalHandler(
-		app.Logger(),
-		app.txConfig.TxDecoder(),
-		mempool,
-	)
-	app.BaseApp.SetPrepareProposal(proposalHandlers.PrepareProposalHandler())
-	app.BaseApp.SetProcessProposal(proposalHandlers.ProcessProposalHandler())
-	app.BaseApp.SetAnteHandler(anteHandler)
+	// // Set the proposal handlers on the BaseApp along with the custom antehandler.
+	// proposalHandlers := abci.NewProposalHandler(
+	// 	app.Logger(),
+	// 	app.txConfig.TxDecoder(),
+	// 	mempool,
+	// )
+	// app.BaseApp.SetPrepareProposal(proposalHandlers.PrepareProposalHandler())
+	// app.BaseApp.SetProcessProposal(proposalHandlers.ProcessProposalHandler())
+	// app.BaseApp.SetAnteHandler(anteHandler)
 
-	// Set the custom CheckTx handler on BaseApp.
-	checkTxHandler := abci.NewCheckTxHandler(
-		app.BaseApp,
-		app.txConfig.TxDecoder(),
-		keyshareLane,
-		anteHandler,
-		ChainID,
-	)
-	app.SetCheckTx(checkTxHandler.CheckTx())
+	// // Set the custom CheckTx handler on BaseApp.
+	// checkTxHandler := abci.NewCheckTxHandler(
+	// 	app.BaseApp,
+	// 	app.txConfig.TxDecoder(),
+	// 	keyshareLane,
+	// 	anteHandler,
+	// 	ChainID,
+	// )
+	// app.SetCheckTx(checkTxHandler.CheckTx())
 
-	// ---------------------------------------------------------------------------- //
-	// ------------------------- End Custom Code ---------------------------------- //
-	// ---------------------------------------------------------------------------- //
+	// // ---------------------------------------------------------------------------- //
+	// // ------------------------- End Custom Code ---------------------------------- //
+	// // ---------------------------------------------------------------------------- //
 
 	// this line is used by starport scaffolding # stargate/app/keeperDefinition
 
