7777438a8c0e16ce370982e8c21e1f687db9a439
p0p3ye <40417611+p0p3yee@users.noreply.github.com>
2024-07-29 16:40:21 -0400

    Update comments

--- x/keyshare/migrations/v2/store.go
+++ x/keyshare/migrations/v2/store.go
@@ -7,12 +7,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// MigrateStore migrates the x/bank module state from the consensus version 3 to
-// version 4. Specifically, it takes the parameters that are currently stored
-// and managed by the x/params module and stores them directly into the x/bank
-// module state.
+// MigrateStore migrates the x/keyshare module state from the consensus version 1 to version 2.
 func MigrateStore(ctx sdk.Context, storeService store.KVStoreService, cdc codec.BinaryCodec) error {
-	// SendEnabled is migrated to the x/bank module store, so delete from the params
 	currParams := types.NewParams(
 		463000,
 		[]string{"fairy1r6q07ne3deq64ezcjwkedcfe6669f0ewpwnxy9"},
