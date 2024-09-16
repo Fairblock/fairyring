aedad4a6e79e45fcf33d25d3fce585e788bc18fa
ap0calypse644 <nibirpal1994@gmail.com>
2024-09-16 17:39:50 +0530

    adding validation on scep pubkey

--- x/pep/types/message_get_private_keyshares.go
+++ x/pep/types/message_get_private_keyshares.go
@@ -1,7 +1,11 @@
 package types
 
 import (
+	"encoding/base64"
+	"errors"
+
 	errorsmod "cosmossdk.io/errors"
+	"github.com/btcsuite/btcd/btcec/v2"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
@@ -21,5 +25,28 @@ func (msg *MsgGetPrivateKeyshares) ValidateBasic() error {
 	if err != nil {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
+
+	if !isValidSecp256k1PubKey(msg.SecpPubkey) {
+		return errors.New("invalid scep256k1 pubkey")
+	}
+
 	return nil
+}
+
+// Function to validate the secp256k1 public key
+func isValidSecp256k1PubKey(pubKeyBase64 string) bool {
+	// Decode the base64 public key
+	pubKeyBytes, err := base64.StdEncoding.DecodeString(pubKeyBase64)
+	if err != nil {
+		return false
+	}
+
+	// Try to parse the public key
+	_, err = btcec.ParsePubKey(pubKeyBytes)
+	if err != nil {
+		return false
+	}
+
+	// If no error, the public key is valid
+	return true
 }
