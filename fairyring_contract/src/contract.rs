9d9b6d925e12f7e3d9dafe481907162c623ea68a
bowenyou <bowenyou@users.noreply.github.com>
2024-10-01 11:14:17 -0400

    fix request encoding

--- fairyring_contract/src/contract.rs
+++ fairyring_contract/src/contract.rs
@@ -1,8 +1,12 @@
 // contract.rs
-use cosmwasm_std::{attr, entry_point, to_json_binary, to_json_string, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult};
-use crate::msg::{ExecuteContractMsg, QueryMsg, QueryResponse, InstantiateMsg};
+use crate::msg::{ExecuteContractMsg, InstantiateMsg, QueryMsg, QueryResponse};
 use crate::state::STORED_DATA;
+use cosmwasm_std::{
+    attr, entry_point, to_json_binary, to_json_string, Binary, Deps, DepsMut, Env, MessageInfo,
+    Response, StdError, StdResult,
+};
 use fairblock_proto::fairyring::pep::QueryDecryptDataRequest;
+use prost::Message;
 
 #[entry_point]
 pub fn execute(
@@ -12,7 +16,7 @@ pub fn execute(
     msg: ExecuteContractMsg,
 ) -> StdResult<Response> {
     // Store the data
-    
+
     // Check if identity is a non-empty string
     if msg.identity.trim().is_empty() {
         return Err(StdError::generic_err("Identity cannot be empty"));
@@ -22,29 +26,22 @@ pub fn execute(
     let identity = msg.identity;
 
     STORED_DATA.save(
-        deps.storage, 
-        &identity, 
-        &(msg.pubkey.clone(), msg.aggr_keyshare.clone()),  // Use `.clone()` to avoid moving the values
+        deps.storage,
+        &identity,
+        &(msg.pubkey.clone(), msg.aggr_keyshare.clone()), // Use `.clone()` to avoid moving the values
     )?;
-    
+
     // Return a response
-    Ok(Response::new()
-        .add_attributes(vec![
-            attr("action", "store_data"),
-            attr("identity", identity),
-            attr("pubkey", msg.pubkey),
-            attr("aggr_keyshare", msg.aggr_keyshare),
-        ])
-    )
+    Ok(Response::new().add_attributes(vec![
+        attr("action", "store_data"),
+        attr("identity", identity),
+        attr("pubkey", msg.pubkey),
+        attr("aggr_keyshare", msg.aggr_keyshare),
+    ]))
 }
 
-
 #[entry_point]
-pub fn query(
-    deps: Deps<QueryMsg>,
-    _env: Env,
-    msg: QueryMsg,
-) -> StdResult<Binary> {
+pub fn query(deps: Deps<QueryMsg>, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
         QueryMsg::GetStoredData { identity } => {
             if identity.trim().is_empty() {
@@ -99,7 +96,6 @@ pub fn instantiate(
     Ok(Response::new().add_attribute("method", "instantiate"))
 }
 
-
 // Function to query the `DecryptData` RPC from your `pep` module
 pub fn query_pep_decrypt(
     deps: Deps<QueryMsg>,
@@ -115,7 +111,10 @@ pub fn query_pep_decrypt(
     };
 
     // Send the query
-    let raw_response: Binary = deps.querier.query_grpc("/fairyring.pep.Query/DecryptData".to_string(), to_json_binary(&request)?)?;
-    
+    let raw_response: Binary = deps.querier.query_grpc(
+        "/fairyring.pep.Query/DecryptData".to_string(),
+        Binary::new(request.encode_to_vec()),
+    )?;
+
     Ok(raw_response)
 }
