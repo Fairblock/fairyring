8d69e6af58b757301befd490cc9f1927d4946b75
YadlaMani <yadlamaniymn2005@gmail.com>
2026-05-22 11:54:38 +0530

    fixed fmt issue

--- x/keyshare/client/cli/tx.go
+++ x/keyshare/client/cli/tx.go
@@ -13,7 +13,6 @@ import (
 
 var DefaultRelativePacketTimeoutTimestamp = uint64((time.Duration(10) * time.Minute).Nanoseconds())
 
-
 // GetTxCmd returns the transaction commands for this module.
 func GetTxCmd() *cobra.Command {
 	cmd := &cobra.Command{
